9a0690d7975d3c0e786702f3427453732d6d6bac
Cole Hodler <cole.hodler@gmail.com>
2025-04-28 18:25:08 +0000

    update spotify redirect URI

--- backend/src/main/java/com/spotifyanalyzer/backend/authservice/SpotifyService.java
+++ backend/src/main/java/com/spotifyanalyzer/backend/authservice/SpotifyService.java
@@ -45,7 +45,7 @@ public class SpotifyService {
         //System.out.println("Redirect URI: " + spotifyConfig.getRedirectUri());
 
         // set our redirect to the frontend callback (THIS MUST BE THE SAME AS WHATEVER IS IN SPOTIFY DASHBOARD!!!)
-        String redirectUri = "http://localhost:3000/callback";
+        String redirectUri = spotifyConfig.getRedirectUri();;
 
         String authUrl = String.format(
                 "https://accounts.spotify.com/authorize?response_type=code&client_id=%s&scope=%s&redirect_uri=%s&state=%s",
@@ -64,7 +64,7 @@ public class SpotifyService {
         headers.setContentType(MediaType.APPLICATION_FORM_URLENCODED);
 
         // IMPORTANT: MUST BE THE SAME REDIRECT AS IN getAuthorisationURL!!
-        String redirectUri = "http://localhost:3000/callback";
+        String redirectUri = spotifyConfig.getRedirectUri();;
 
         MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
         body.add("grant_type", "authorization_code");
